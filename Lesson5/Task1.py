c50c5519e27c2e4f238ae8d94ff2bdbf57ffe9ca
galina.nudga <galina.s_86@mail.ru>
2022-10-09 13:57:14 +0500

    Add Lesson5/Task1

---
+++ Lesson5/Task1.py
@@ -0,0 +1,28 @@
+# 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
+#  В тексте используется разделитель пробел.
+
+from random import choices
+
+
+def text_new(n, word):
+    new_line = ''
+    new_list = []
+    for i in range(n):
+     a = choices(word, k=3)
+     new_list.append(''.join(a))
+     new_line += f'{new_list[i]} '
+    return new_line
+
+
+def del_from_text(word_d:str, in_line:str):
+    m_list = in_line.split()
+    n_line = ''
+    for w in m_list:
+     if w != word_d:
+        n_line += f'{w} '
+    return n_line
+
+
+line= text_new(int(input('Введите количество слов в строке: ')), 'абв')
+print(line)
+print(del_from_text('абв',line))
